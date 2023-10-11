f1a7c762c857a08e8fad3914accb7e488b78180b
Royr9 <royramati99@gmail.com>
2023-10-11 12:31:48 +0200

    commit

--- Front-end/src/index.tsx
+++ Front-end/src/index.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 // import App from './App';
-import Website from './Website';
+import Website from "./Website";
 
 //css
-import "./css/Index.css"
+import "./css/Index.css";
 
 import "./css/library/animations.css";
 import "./css/library/layouts.css";
 import "./css/library/styles.css";
 
-
-
-
-
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <Website />
   </React.StrictMode>
 );
-
-
