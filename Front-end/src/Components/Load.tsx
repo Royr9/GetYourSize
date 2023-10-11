294cc7363c85a26aecbcf583e2ac5eceea04745c
Royr9 <royramati99@gmail.com>
2023-10-11 19:49:01 +0200

    fixed mobile size inputs size 3

--- Front-end/src/Components/Load.tsx
+++ Front-end/src/Components/Load.tsx
@@ -9,23 +9,18 @@ export default function Load() {
       <Logo />
 
       <div className="load-animation">
-        <video
-          autoPlay
-          loop
-          width={121}
-          height={120}
-          src="/media/load-animation.mp4"
-        ></video>
+        {
+          <LoopRoundedIcon
+            sx={{ fontSize: 100 }}
+            className="animate rotate animate--infinite animate--slow"
+          />
+        }
+
         <h3>Calculating...</h3>
       </div>
     </div>
   );
 }
 /* <span className="circular-loader-container ">
-        {
-          <LoopRoundedIcon
-            sx={{ fontSize: 100 }}
-            className=" rotate animate--infinite animate--slow"
-          />
-        }
+       
       </span> */
